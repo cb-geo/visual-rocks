19983f3dfcbaf43eaf1e17f4bd71c056a4a94d52
mhgardner <shellshocked2003@gmail.com>
2015-08-06 16:52:26 -0700

    First shot at writing poly data. Not yet tested, need to add functionality in sparkRocks to output JSON in expected format

--- src/vtkWriter.py
+++ src/vtkWriter.py
@@ -22,23 +22,46 @@ inputFile = str(sys.argv[1])
 with open(inputFile, 'r') as f:
     data = json.load(f, object_hook = JSONObject)
 
-
-## MODIFY THIS: TREAT AS IF INPUT FILE JUST HAS LIST FOR EACH OF THESE AS PART OF THE DICTIONARY
-
-# Find number faces for all blocks
-ncells = 0
-for i in range(0, len(data.blocks)):
-    ncells = ncells + len(data.blocks[i])
-
-# Generate list of normal vectors
-normals = ()
-for i in range(0, len(data.blocks)):
-    for j in range()
-
 # Initialize vtk file
 vtkBlocks = VtkFile("./blocks", VtkPolyData)
-vtkBlocks.openPiece(start = None, end = None,
-                    npoints = len(data.vertices), ncells, nverts = len(data.vertices),
-                    nlines = None, nstrips = None, npolys = len(data.blocks))
 
+for i in range(0, len(data) - 1): 
+    # Extract data for vtk file creation
+    npoints = data.vertexCount(i)
+    pointIDs = data.vertexIDs(i)
+    vertices = data.vertices(i)
+    npolys = data.blockCount(i)
+    normals = data.normals(i)
+    connectivity = data.connectivity(i)
+    offsets = data.offsets(i)
+    
+    vtkBlocks.openPiece(start = None, end = None,
+                        npoints, ncells = None, nverts = None,
+                        nlines = None, nstrips = None, npolys)
+    
+    # Point data
+    vtkBlocks.openElement("Points")
+    vtkBlocks.addData("Coordinates", vertices)
+    vtkBlocks.closeData("Points")
+    vtkBlocks.openData("Point", scalars = "pointIDs")
+    vtkBlocks.addData("pointIDs", pointIDs)
+    vtkBlocks.closeData("Point")
 
+    # Cell data
+    vtkBlocks.openData("Cell", normals = "normals")
+    vtkBlocks.addData("normals", normals)
+    vtkBlocks.closeData("Cell")
+    
+    # Poly data
+    vtkBlocks.openElement("Polys")
+    vtkBlocks.addData("connectivity", connectivity)
+    vtkBlocks.addData("offsets", offsets)
+    vtkBlocks.closeData("Polys")
+
+    # Append data
+    vtkBlocks.appendData(vertices)
+    vtkBlocks.appendData(pointIDs)
+    vtkBlocks.appendData(normals)
+    vtkBlocks.appendData(connectivity).appendData(offsets)
+
+vtkBlocks.save()
