367a8dfc0e64494552cd7c72c370d78d955aa9b0
mhgardner <shellshocked2003@gmail.com>
2015-08-13 17:06:55 -0700

    Appears to be working correctly. There are some issues on the sparkRocks side that need to be addressed before this can be fully verified.

--- src/vtkWriter.py
+++ src/vtkWriter.py
@@ -25,23 +25,30 @@ with open (inputFile, 'r') as f:
     for line in f:       
         data.append(json.loads(line, object_hook = JSONObject))
 
-# with open(inputFile, 'r') as f:
-#     data = json.load(f, object_hook = JSONObject)
-
-# Initialize vtk file
+# Initialize vtk file and open polygon data
 vtkBlocks = VtkFile("./blocks", VtkPolyData)
+vtkBlocks.openElement("PolyData")
 
 print len(data)
 
-for i in range(0, len(data) - 1): 
+for i in range(0, len(data)): 
     # Extract data for vtk file creation
-    npoints = len(data[i].vertices)
+    npoints = len(data[i].vertexIDs)
     pointIDs = np.asarray(data[i].vertexIDs)
     vertices = np.asarray(data[i].vertices)
     npolys = np.asarray(data[i].faceCount)
     normals = np.asarray(data[i].normals)
     connectivity = np.asarray(data[i].connectivity)
     offsets = np.asarray(data[i].offsets)
+    cellIDs = np.arange(0, npolys)
+    print "Vertices: " 
+    print vertices
+    print "Connectivity: " 
+    print connectivity
+    print "Offsets: " 
+    print offsets
+    print "Normals:"
+    print normals
     
     vtkBlocks.openPiece(start = None, end = None,
                         npoints = npoints, ncells = None, nverts = None,
@@ -49,27 +56,49 @@ for i in range(0, len(data) - 1):
     
     # Point data
     vtkBlocks.openElement("Points")
-    vtkBlocks.addData("Coordinates", vertices)
-    vtkBlocks.closeData("Points")
+    vtkBlocks.addHeader("points", vertices.dtype.name, len(pointIDs), 3)
+    vtkBlocks.closeElement("Points")
+    # vtkBlocks.openElement("Points")
+    # vtkBlocks.addData("points", vertices)
+    # vtkBlocks.closeElement("Points")
     vtkBlocks.openData("Point", scalars = "pointIDs")
     vtkBlocks.addData("pointIDs", pointIDs)
     vtkBlocks.closeData("Point")
 
     # Cell data
-    vtkBlocks.openData("Cell", normals = "normals")
-    vtkBlocks.addData("normals", normals)
+    vtkBlocks.openData("Cell", scalars = "cellIDs", normals = "normals")
+    vtkBlocks.addHeader("cellIDs", cellIDs.dtype.name, npolys, 1)
+    vtkBlocks.addHeader("normals", normals.dtype.name, npolys, 3)
+    # vtkBlocks.addData("normals", normals)
     vtkBlocks.closeData("Cell")
     
     # Poly data
     vtkBlocks.openElement("Polys")
     vtkBlocks.addData("connectivity", connectivity)
     vtkBlocks.addData("offsets", offsets)
-    vtkBlocks.closeData("Polys")
+    vtkBlocks.closeElement("Polys")
+    
+    vtkBlocks.closePiece()
+
+# Close polygon data
+vtkBlocks.closeElement("PolyData")
+
+# Append data for each polygon
+for i in range(0, len(data)): 
+    # Extract data for each piece
+    pointIDs = np.asarray(data[i].vertexIDs)
+    vertices = np.asarray(data[i].vertices)
+    normals = np.asarray(data[i].normals)
+    connectivity = np.asarray(data[i].connectivity)
+    offsets = np.asarray(data[i].offsets)
+    cellIDs = np.arange(0, npolys)
 
     # Append data
     vtkBlocks.appendData(vertices)
     vtkBlocks.appendData(pointIDs)
+    vtkBlocks.appendData(cellIDs)
     vtkBlocks.appendData(normals)
     vtkBlocks.appendData(connectivity).appendData(offsets)
 
+# Save vtk file
 vtkBlocks.save()
